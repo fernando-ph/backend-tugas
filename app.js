4c07ca21e0c207774a420a06394dfeb3c95bae2a
fernando-ph <fernandolumbangaol11@gmail.com>
2021-05-18 23:04:25 +0700

    change database to MySql from SQL Server:

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const {Conneciton, Request} = require('./db');
+const express = require('express');
+const app = express();
+const port = 3000;
+const {connection} = require('./db');
 
 app.get('/products', (req, res) => {
-  res.send('Hello World!');
+    connection.connect();
+    const products = [];
+    connection.query('SELECT name, stock FROM products', function (err, rows, fields) {
+        if (err) throw err
+        // console.log('The solution is: ', row)
+        // res.json(rows);
+
+        rows.forEach(product => {
+            // products.push(product);
+            console.log(product.name, product.stock);
+        });
+    });
+    connection.end()
+    res.send('Berhasil');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
--- db.js
+++ db.js
@@ -1,16 +1,10 @@
-var Connection = require('tedious').Connection
-var Request = require('tedious').Request
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'backend-tugas',
+  port: 3306
+})
 
-var config = {
-  server: 'localhost',
-  authentication: {
-    type: 'default',
-    options: {
-      userName: 'DESKTOP-S61FQGG/nandoterorisLG', // update me
-      password: '' // update me
-    }
-  }
-}
-
-module.exports.Connection = new Connection(config);
-module.exports.Request = Request;
+module.exports.connection = connection;
