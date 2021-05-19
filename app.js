949d46c1a7d5aa83ab05b00103c0cfb46574e6be
fernando-ph <fernandolumbangaol11@gmail.com>
2021-05-19 15:51:22 +0700

    cretae the endpoint to provide get products

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ const app = express();
 const port = 3000;
 const {connection} = require('./db');
 
-app.get('/products', (req, res) => {
-    connection.connect();
-    const products = [];
-    connection.query('SELECT name, stock FROM products', function (err, rows, fields) {
-        if (err) throw err
-        // console.log('The solution is: ', row)
-        // res.json(rows);
 
-        rows.forEach(product => {
-            // products.push(product);
-            console.log(product.name, product.stock);
-        });
+app.get('/products', async function(req, res) {
+    connection.query("SELECT name, stock FROM products", function(err, rows, fields) {
+        if (err) {
+            res.status(500).send({
+                status: 500,
+                message: 'Terjadi kesalahan'
+            });
+        }
+        if (rows.length !== 0) {
+            res.json({
+                status: 200,
+                rows
+            });
+        } else if (rows.length === 0) {
+            res.status(404).send({
+                status: 404,
+                message: 'Data product tidak ada.',
+            });
+        }
     });
-    connection.end()
-    res.send('Berhasil');
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
